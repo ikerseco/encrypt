b04bb1452d6e8d165288a8f4c8c49e130b41dbda
ikerseco <garrantsitsua@gmail.com>
2017-10-30 00:10:53 +0100

    primer comit

---
+++ codificar.py
@@ -0,0 +1,217 @@
+import os
+es = "vx66cLsxoñU3ñp1;MLf*ÑlkBt3&dmqmFYY4iopCÑwÇs?9Py¿V9KKN00cRPF?jgR;ydwj2QÑ0w*!0kKlz2A$aO8aÑVoYsJU7QiI2q"
+q = "Qm50QdjO@V7&9/QZCbrVM%VijS9%3;!xY3%b9Ñ*Çpg1Z1L^JbkSgs28@LMq¿ZnXmVR2uvyh5y?GU;M@RIWl33Z0zhbsXb%&WÑLS/"
+w = "VGUsaVi!lnnoQY6@L!AP/2&%/Mb&zsx8?26w5sXhD*$rD*VñCa7ZWB!DI2B¿7vArZ@FRtHWJMx4m2ñ¿^?f3bGzCxPPlmbCofR;cY"
+e = "UfGzaS3b7s¿/ñnLUu*ur4O?n¿KJXÇ6¿sSbhYUsQu78d&PY2A&Ck9w7uNcg1;xsc?rmKMyñ!k@Qbq9rnaO0b23S4P9UmMz¿w!3!Gn"
+r = "ñQBJÑs6YI2&FZX5mAwm7W¿BqmaInzVwk;G*B7w6D;rvtpIrbjp//NKmWhJidzmruNzymCy4ÇadBwmuyochfVcopVVCooo40Yk!VÇ"
+t = "fW!wjj@&Mh8C^oZD$XZDhm$!Zw^R&ngDNwh%dI3uiFHJP9mVGgqz&fRJ*mfNnosX/dI;yJWj$BKñcilu;MI?YNBXm$V&pBz?dRF7"
+y = "uB3FNZU4rJHphmJkIi!dC;SÇ*ñF8&RPswpaysXbN¿2xpasB?BkiLIpGgFnjPGDVZ3319/6nbjOt3;UF4zfk!fnpvttFLcJaxCl^w"
+u = "PjLG9ugKX¿wGM!F!zZVp1&mas4%*sMGddoBL1i^iPBY¿yYzfm8$*H$^/iKVt$Ç5P4PcCnd8wWKlX%3i8q3!nxYJICDi0rlaDt0qn"
+i = "YWRfPP$iPgÑasv1ñ?HV@$A3j!5RHG@/vzz7iuÑFbopX;HP41sJHflGXohuLlxiAFu/uiñUQ?8;$RupCfxRHBD@R2Dd1vAv1WJ?YQ"
+o = "S?h6G^GwV/pcg3NsknMk8zñgÑ$Kgx¿swuHHVFGGYW&HvglK08@N¿Bpb*uLuk%ym^QWxA@qfggjiA3;OVK8xlv¿nvbStq*S^8FYyS"
+p = "vMYdk5/SCysñO9d;0@sfsIQhBco$vhñ5vqPÑ6bWJt2@9N71szHkfrZD3aMRGi/8dkHBMgW53wQWq5JGDwx96HKm0s7K3XyH2/N&I"
+a = "Gg15ANhM6NlB$htu7ii1%kFQMPjfIfl@rMs52Lp^lñoO@Ñ%JG$RlMxwQÑAwFvP3!olbrWqNl/ÇzÑjXS/7By¿/Ojx364UYUDoML2?"
+s = "&;tA*xyJbX@k6ZK¿vY3tvIñ1Ipgq8FgQSaVYfÇa?nvWabLFVi*lQñ&I1CudD2ZM$RiSiUF^H1Z3G556mCz$4$qul¿QDk;YzMq4y9"
+d = "2yDMLyk@t*UsqJFyoLñ2¿J!JAW@sVM;uZG4OZNmXZJONl8JB0/o!3?rrÑUMBGvÑQyuply¿F*ÑtcVBIX?GZñjsl7YfWrAxm1UfNQg"
+f = "nVhhgypBx6ybuCs23Og*;bnbs/7lV1!n$Suw5JR554uVWHsv?yuMvb87Gjñ%xFg40HDLlmlñqqj1IÑj3mqB0DñAmahIzHO3%iÑh*"
+g = "RyI^q32aXÑVXCMXG@z3;2^moZW6RXyP¿!RgÑfS&;X^yzn%XAdPQDh;zcR/%3QffMXÇv&mxN*7*pZLnjCÑr^GC0?1x$NC7^n*500M"
+h = "*%AowQrNtmVxRtw!@kr@sa*;WlHÇ%yaYjAw1BZN2I5ñzt!100HR0sj^RWabVK3bHgMj4hS@J5!H;OQRBvwvsYzxcRbup$zRHoLXW"
+j = "ñV*2MglJA6uj&UyoOz3Wtn@RQJV8cZPY5lQy5/89K2ñAd6lnK2wXU^sopxJJiI9CAZXlZP*^3MVvW%Vgafñjo4H7ujPMBIa;0*Uq"
+k = "jAXy5OX$gSmdgv/c@/^YOrWqQjCc¿jhbvk*UZ9R?85Z3vu120xM^m2tuX/w67a@r&@^Y%J8WL2AxFro5oA2SWlÇm@7FmUj25V;ng"
+l = "0cYHnVO*8jP!yd;rvÇAoSQ9V?Bd0?qñ@l7ñd!4R6JfhRnaBCZDBmj&ñ6k$VblFfrs^ñRñVpWzÑIJn$FbzX6¿*uPns75HSQkGUHqG"
+ñ = "W2D%3y^1%bY6VMq?2CzZ/ugVtht^IP8Pñ@^&¿o;v0NBZFV3W5ñ7hZ&w1MS^ñmv?1/VIOCat&C%5pA3$&l7^FW/x9a8^8fLÑKrSrG"
+z = "4GÑJmKn&UR9nJm!4Çu*RRc15jÑjRhñÑAYo^fYFn!UIqwxdSKcñ2@D2f%^W@svkqW@NGODñ¿DO8mN5yV*U4pobIMwXH8M44AVZ!PF"
+xi = "UoLÑZ//M!ly6d4gfy3/A5@XBPa!8ñxaZlg/PDAvt?ñIQGMW^^jgdI4r;JNp3DXwcvGlnHFiqdjCN&lbXFFxlpWfiñ¿%0C1?23Fm^"
+c = "rÑhp¿rHd5C?nOKoOCoPjd!B!9?YPa3ZtJÑlÑcÑ9!SdjñYAN&jaLF3cZN^ñqAxzmIJ@¿q6k0vÑGqiS2yD*8I!Yl@9*GC4yR3t1cci"
+v = "@s?pchR9CSBocqxp3ZLIC;usubRNCg/Ç?F64uWYcKh;4sÑ8ruSWWl!r9VO3lNHGrro?;VñkQL!nlg4r2Cag73%XtZÑ;2%D¿Wc¿c@"
+b = "qNhPWA!¿9kxmgFY!yN¿dgXñN¿tM$3S2I;hyUm&kqMa9ArX5Cx4ÇWRyj3¿ñ1xBoñI@kW¿d$C2hW^uUNwhZNlWqsÇa8Km6MlQ@A¿h4"
+n = "0¿t2nkñaN&mU5F/Zl$1Çy6now2jH6ya?s*%^*a5cUAQb6pJo2rR^iBrIWÑSXOy5!cNA6f^Zjabk¿;J5@i2Fkx??jF0GNy@kIddN%"
+m = "ytis65X4?ÑAht$F6?ZLW0ñÇaq36&WQxfLo8r2D$i^^ÇG$ChlfdDCNÇW5achZbtqyg/ltÇ¿5P4FV9tGK1Ñ7ñfSPYC*J90?c^2QUhL"
+
+def iniciar():
+ kodi_deskodi = input("kodifikatu edo deskodificatu ko/des:")
+ os.chdir("D:\phyton\programak\kodifikar");
+ if kodi_deskodi == "ko":
+  fichategia = open ('seco.txt','rt')
+  irakurri =  fichategia.read()
+  lotura = kodifikatu(irakurri)
+  fichategia.close()
+  idatzi(lotura)
+ elif kodi_deskodi == "des":
+  fichategia = open ('seco.txt','rt')
+  irakurri =  fichategia.read()
+  lotura = deskodifikatu(irakurri)
+  fichategia.close()
+  idatzi(lotura)
+ 
+def kodifikatu(irakurri):
+ luzehera = len(irakurri)
+ lotura = ""
+ for x in range(luzehera):
+  letrak = irakurri[x]
+  if letrak == " ":
+   lotura += es
+  if letrak == "q":
+   lotura += q
+  if letrak == "w":
+   lotura += w
+  if letrak == "e":
+   lotura += e 
+  if letrak == "r":
+   lotura += r 
+  if letrak == "t":
+   lotura += t
+  if letrak == "y":
+   lotura += y
+  if letrak == "u":
+   lotura += u
+  if letrak == "i":
+   lotura += i
+  if letrak == "o":
+   lotura += o
+  if letrak == "p":
+   lotura += p
+  if letrak == "a":
+   lotura += a 
+  if letrak == "s":
+   lotura += s 
+  if letrak == "d":
+   lotura += d
+  if letrak == "f":
+   lotura += f 
+  if letrak == "g":
+   lotura += g
+  if letrak == "h":
+   lotura += h 
+  if letrak == "j":
+   lotura += j
+  if letrak == "k":
+   lotura += k
+  if letrak == "l":
+   lotura += l 
+  if letrak == "ñ":
+   lotura += ñ
+  if letrak == "z":
+   lotura += z
+  if letrak == "c":
+   lotura += c 
+  if letrak == "v":
+   lotura += v 
+  if letrak == "b":
+   lotura += b
+  if letrak == "n":
+   lotura += n
+  if letrak == "m":
+   lotura += m
+  if letrak == "x":
+   lotura += xi  
+ return lotura
+
+def deskodifikatu(irakurri):
+ luzehera = len(irakurri)
+ le = "" 
+ lotura = ""
+ for x in range(luzehera):
+  letrak = irakurri[x]
+  le += letrak
+  if le == s:
+   lotura += "s"
+   le = ""
+  if le == c:
+   lotura += "c"
+   le = ""
+  if le == m:
+   lotura += "m"
+   le = ""
+  if le == es:
+   lotura += " "
+   le = ""
+  if le == q:
+   lotura += "q"
+   le = ""
+  if le == w:
+   lotura += "w"
+   le = ""
+  if le == e :
+   lotura += "e"
+   le = ""
+  if le == r:
+   lotura += "r"
+   le = ""
+  if le == t:
+   lotura += "t"
+   le = ""
+  if le == y:
+   lotura += "y"
+   le = ""
+  if le == u:
+   lotura += "u"
+   le = ""
+  if le == i:
+   lotura += "i"
+   le = ""
+  if le == o:
+   lotura += "o"
+   le = ""
+  if le == p:
+   lotura += "p"
+   le = ""
+  if le == a:
+   lotura += "a"
+   le = ""
+  if le == d:
+   lotura += "d"
+   le = ""
+  if le == f:
+   lotura += "f"
+   le = ""
+  if le == g:
+   lotura += "g"
+   le = ""
+  if le == h:
+   lotura += "h"
+   le = ""
+  if le == j:
+   lotura += "j"
+   le = ""
+  if le == k:
+   lotura += "k"
+   le = ""
+  if le == l:
+   lotura += "l"
+   le = ""
+  if le == ñ:
+   lotura += "ñ"
+   le = ""
+  if le == z:
+   lotura += "z"
+   le = ""
+  if le == x:
+   lotura += "x"
+   le = ""
+  if le == c:
+   lotura += "c"
+   le = ""
+  if le == v:
+   lotura += "v"
+   le = ""
+  if le == b:
+   lotura += "b"
+   le = ""
+  if le == n:
+   lotura += "n"
+   le = ""
+  if le == m:
+   lotura += "m"
+   le = ""
+ return lotura
+ 
+ 
+def idatzi(lotura):
+ os.chdir("D:\phyton\programak\kodifikar")
+ fichategia = open ('seco.txt','wt')
+ fichategia.write(lotura)
+ fichategia.close()
+ 
+#programa
+iniciar()
